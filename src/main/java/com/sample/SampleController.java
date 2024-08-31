5f2cc6d1ff45c7031b5750a9c11d0c74094c0f5c
SatyaCh0111 <satya@cmmn.com.au>
2024-08-31 20:17:45 +0530

    java backend

---
+++ src/main/java/com/sample/SampleController.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping("/test")
+    public String sampleTest(){
+        return "Sample java test project";
+    }
+}
